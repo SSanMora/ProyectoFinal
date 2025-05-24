75dd8e086acf354afd12a984ba83c413382e71c0
SSanMora <serrano.santiago@correounivalle.edu.co>
2025-05-24 22:54:45 +0000

    He agregado privado y mas funciones

--- ahorcado/include/Juego.h
+++ ahorcado/include/Juego.h
@@ -3,8 +3,15 @@
 #define JUEGO_H
 
 #include <string>
+using namespace std;
 
 class Juego {
+    private:
+    string nombreJugador;
+    int puntuacion;
+    string resultado; // "G" (ganó) o "P" (perdió)
+    string tipoJuego; // "AH" (Ahorcado) o "MEM" (Concentrese)
+
     public:
         virtual void jugar() = 0;
         /* esto hace que todas las clases hijas definan una versión propia del método */
@@ -12,6 +19,14 @@ class Juego {
         /* esto es para que cada juego guarde el historial de forma personalizada */
         virtual ~Juego() {} 
         /* esto es el destructor virtual, asegura que las clases hijas se destruyan correctamente */
+        Juego(string jugador, string tipo);
+        virtual ~Juego();
+        
+        void guardarHistorial();
+
+        string obtenerFechaActual();
+        
+        virtual void mostrarInstrucciones();
 };
 
 /* este "juego.h" es una clase abstracta, 
