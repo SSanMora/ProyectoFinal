916f269c83e5657120c623a77197004d2e85b26d
soku <echeverry.laura@correounivalle.edu.co>
2025-05-31 23:34:29 +0000

    lo elimine

---
+++ ahorcado/build/CMakeFiles/ahorcado.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ahorcado.dir/main.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Ahorcado.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Concentrese.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Historial.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Juego.cpp.o"
+  "ahorcado"
+  "ahorcado.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ahorcado.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
