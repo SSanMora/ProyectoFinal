ba69bd9c5a0f4b8fb98fff419daaca57c6b1d9f7
soku <echeverry.laura@correounivalle.edu.co>
2025-05-31 22:38:38 +0000

    hice la clase ahorcado

---
+++ build/ahorcado/CMakeFiles/ahorcado.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/ProyectoFinal/ahorcado/main.cpp" "/workspaces/ProyectoFinal/build/ahorcado/CMakeFiles/ahorcado.dir/main.cpp.o"
+  "/workspaces/ProyectoFinal/ahorcado/src/Ahorcado.cpp" "/workspaces/ProyectoFinal/build/ahorcado/CMakeFiles/ahorcado.dir/src/Ahorcado.cpp.o"
+  "/workspaces/ProyectoFinal/ahorcado/src/Concentrese.cpp" "/workspaces/ProyectoFinal/build/ahorcado/CMakeFiles/ahorcado.dir/src/Concentrese.cpp.o"
+  "/workspaces/ProyectoFinal/ahorcado/src/Historial.cpp" "/workspaces/ProyectoFinal/build/ahorcado/CMakeFiles/ahorcado.dir/src/Historial.cpp.o"
+  "/workspaces/ProyectoFinal/ahorcado/src/Juego.cpp" "/workspaces/ProyectoFinal/build/ahorcado/CMakeFiles/ahorcado.dir/src/Juego.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ahorcado/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/ahorcado/CMakeFiles/ahorcado.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../bin/ahorcado"
+  "../bin/ahorcado.pdb"
+  "CMakeFiles/ahorcado.dir/main.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Ahorcado.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Concentrese.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Historial.cpp.o"
+  "CMakeFiles/ahorcado.dir/src/Juego.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ahorcado.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
